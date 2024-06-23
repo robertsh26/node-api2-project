92bda8f1c520209cb8bf47f55c7a79495b4b6521
Robert Shepherd <robertsh26@outlook.com>
2024-06-23 13:35:24 -0700

    ok

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -56,8 +56,24 @@ router.post('/', (req, res) => {
         })
     }
 })
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteID = await Post.findById(req.params.id)
+        if (!deleteID) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(deleteID)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        })
+    } 
 })
 router.put('/:id', (req, res) => {
 
